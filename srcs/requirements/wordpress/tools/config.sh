3c61263426f1d1b5c5ece5e80247135b4e76c148
yrimah <rimahyassine.pro@gmail.com>
2023-11-05 13:31:26 -0500

    adding script for wordpress installation

---
+++ srcs/requirements/wordpress/tools/config.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+apt-get install curl -y
+
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+wp cli update
+
+mkdir /var/www/html
+
+wp core download --path=/var/www/html/ --allow-root
+
+wp core config --dbname=wordpress --dbuser=yrimah42 --dbpass=Yrimah42*ri@@ --path=/var/www/html/ --allow-root
+
+wp core install --url=yrimah.42.fr --title=ineption --admin_user=yrimah42 --admin_password=Yrimah42*ri@@ --admin_email=yrimah42@gmail.com --path=/var/www/html
