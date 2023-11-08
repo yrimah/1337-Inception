2c94c5ef50624454ef82e45a443f479e4812d8f9
rootYrimah42 <root@gmail.com>
2023-11-08 08:55:54 -0500

    MPART:FINISHED

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -19,7 +19,7 @@ if [ ! -f "$FILE" ]; then
 
 	wp core config --dbname=$MYSQL_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_DB_HOST --path=$WP_PATH --allow-root
 
-	wp core install --url=$DOMAIN_NAME --title='inception' --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --path=$WP_PATH --allow-root
+	wp core install --url=https://$DOMAIN_NAME --title='inception' --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --path=$WP_PATH --allow-root
 
 	wp user create $WP_USER $WP_USER_EMAIL --role='author' --user_pass=$WP_USER_PASSWORD --path=$WP_PATH --allow-root
 else
