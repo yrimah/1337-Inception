e8ea0b250e09f8700bdf7b96944866da6f5c2e71
rootYrimah42 <root@gmail.com>
2023-11-22 09:13:20 -0500

    extra-service:grafana

---
+++ srcs/requirements/bonus/grafana/conf/config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+apt-get install grafana
+
+service grafana start
---
+++ srcs/requirements/bonus/grafana/tools/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get update && apt-get upgrade
+
+apt-get install -y apt-transport-https software-properties-common wget
