11f9ebb46819cd9edeb8988b73fa9fe2d54eb87d
rootYrimah42 <root@gmail.com>
2023-11-22 13:48:46 -0500

    bonus_left:website,others:done

--- srcs/requirements/bonus/grafana/conf/config.sh
+++ srcs/requirements/bonus/grafana/conf/config.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-sudo mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+rm -rf /usr/share/grafana/conf/defaults.ini
 
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+cp defaults.ini /usr/share/grafana/conf/defaults.ini
 
-apt-get install grafana
-
-service grafana start
+grafana-server --homepath=/usr/share/grafana
--- srcs/requirements/bonus/grafana/tools/install.sh
+++ srcs/requirements/bonus/grafana/tools/install.sh
@@ -2,4 +2,8 @@
 
 apt-get update && apt-get upgrade
 
-apt-get install -y apt-transport-https software-properties-common wget
+apt-get install -y apt-transport-https vim wget gnupg2
+
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add - && echo "deb https://packages.grafana.com/oss/deb stable main" > /etc/apt/sources.list.d/grafana.list
+
+apt-get update && apt-get install -y grafana
